09b6334853fd80c6597c916efd6e4005ce938cca
Imran Zaidi <imran.zaidi@gmail.com>
2018-03-26 17:01:14 -0400

    Remove test emitter

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ function testEmitter() {
   io.emit('torch', { minew: 'foo', verticalDeviations: verticalDeviations });
 }
 
-setInterval(testEmitter, 2000);
+//setInterval(testEmitter, 2000);
 
 
 // Calculate the orientation angle from the acceleration in the given axis.
